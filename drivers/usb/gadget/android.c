696d51a4feeeab46fe590658b265ce9bdd56b1ac
Neil Patel <NPATEL@motorola.com>
2012-02-10 11:37:29 -0600

    IKHSS7-7716: Support RNDIS in BP Tools mode
    When enabling the rndis function in BP tools mode we need to reset
    the endpoint state before re-enumerating. Otherwise we run out of
    endpoints when rebinding due to the number of functions involved.
    
    Change-Id: If0a86da91bd8d4ab8c420f2f8401ebe10e3d5eb4
    Signed-off-by: Neil Patel <NPATEL@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/350911
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Jun-Xiao Chang <jxchang@motorola.com>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -1415,6 +1415,7 @@ static ssize_t enable_store(struct device *pdev, struct device_attribute *attr,
 		/* Cancel pending control requests */
 		usb_ep_dequeue(cdev->gadget->ep0, cdev->req);
 		usb_remove_config(cdev, &android_config_driver);
+		usb_ep_autoconfig_reset(cdev->gadget);
 		dev->enabled = false;
 	} else {
 		pr_err("android_usb: already %s\n",
